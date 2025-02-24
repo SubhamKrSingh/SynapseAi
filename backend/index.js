14f1dbdec016621292d4f719fb4017586b7e9ced
Subham <112950238+SubhamKrSingh@users.noreply.github.com>
2025-02-24 13:21:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import Chat from "./models/chat.js";
 import UserChats from "./models/userChats.js";
 import { ClerkExpressRequireAuth, requireAuth } from "@clerk/clerk-sdk-node";
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001 
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
